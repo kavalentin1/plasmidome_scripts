bb7f78aa08180c5dd418f15e88271c3480a5f380
VALENTIN Karim <59995116+kavalentin1@users.noreply.github.com>
2020-06-19 15:06:32 +0200

    Update run_circular.sh

--- bin/run_circular.sh
+++ bin/run_circular.sh
@@ -9,18 +9,9 @@ if [[ "$#" -ne 2 ]]; then
 	exit 1 
 fi 
 
-tool_dir=$(echo $0 | rev | cut -f 3- -d "/" | rev)
-if [[ $tool_dir == "" ]]; then
-        tool_dir="."
-elif [[ $tool_dir == $0 ]]; then
-        tool_dir=".."
-fi
-BIN=$tool_dir/bin
-
-
 tmp=`mktemp -d -p .`
-echo $tmp
+
 awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);} END {printf("\n");}' $1 > $tmp/oneline.fasta
-$BIN/circular_detection.pl -f $tmp/oneline.fasta -k 100 --only_circular > $2.fasta
+../bin/circular_detection.pl -f $tmp/oneline.fasta -k 100 --only_circular > $2.fasta
 grep "^>" $2.fasta | cut -f 1 -d " " | tr -d ">" > $2.id  
-#rm -r $tmp 
+rm -r $tmp
